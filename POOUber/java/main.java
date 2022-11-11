4169c0539a1995928d5164b55895422492a2d5a6
JJBP55 <jonathanjbello.jb@gmail.com>
2022-11-10 21:45:07 -0400

    Runing Java

--- POOUber/java/main.java
+++ POOUber/java/main.java
@@ -1,5 +1,16 @@
 class Main{
+
     public static void main(String[] args) {
-        
+        Car car = new Car();
+        car.license = "AMQ123";
+        car.driver = "Andres Herrera";
+        car.passengers = 4;
+        car.printDataCar();
+
+        Car car2 = new Car();
+        car2.license = "QWE567";
+        car2.driver = "Andrea Herrera";
+        car2.passengers = 3;
+        car2.printDataCar();
     }
 }
