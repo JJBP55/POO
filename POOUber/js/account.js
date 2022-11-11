e35cdd1f44d98e2fce16b3287cc6071d34158c7d
JJBP55 <jonathanjbello.jb@gmail.com>
2022-11-10 22:45:25 -0400

    Class

--- POOUber/js/account.js
+++ POOUber/js/account.js
@@ -1,7 +1,25 @@
-function Account(){
-    this.id;
-    this.name;
-    this.document;
-    this.email;
-    this.password;
+//Ecmascript 6 
+
+class Account{
+    constructor(name, document){
+        this.id;
+        this.name = name;
+        this.document = document;
+        this.email;
+        this.password;
+    }
 }
+
+
+
+
+
+//Antiguamente
+
+// function Account(name, document){
+//     this.id;
+//     this.name = name;
+//     this.document = document;
+//     this.email;
+//     this.password;
+// }
--- POOUber/python/main.py
+++ POOUber/python/main.py
@@ -1,14 +1,10 @@
 from car import Car
+from account import Account
 
 if __name__ == "__main__":
         print("Hola mundo");
-        car = Car()
-        car.license = "AMS234"
-        car.driver = "Andres herrera"
-        print (vars(car))
-
-        car2 = Car()
-        car2.license = "QWE567"
-        car2.driver = "Andrea herrera"
-        print (vars(car2))
+        
+        car = Car("AMS234", Account("Andres Herrera", "ANDA567"))
+        print(vars(car))
+        print(vars(car.driver))
 
