eef63414613b0fd84de8b2ca9ef9bae06e212811
JJBP55 <jonathanjbello.jb@gmail.com>
2022-11-10 21:54:16 -0400

    Runing Python

--- POOUber/python/main.py
+++ POOUber/python/main.py
@@ -1,2 +1,14 @@
+from car import Car
+
 if __name__ == "__main__":
         print("Hola mundo");
+        car = Car()
+        car.license = "AMS234"
+        car.driver = "Andres herrera"
+        print (vars(car))
+
+        car2 = Car()
+        car2.license = "QWE567"
+        car2.driver = "Andrea herrera"
+        print (vars(car2))
+
